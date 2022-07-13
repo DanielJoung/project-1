605db136ae4b14371fb3e87cb2e6c1a6805507a6
DanielJoung <djoung100@hotmail.com>
2022-07-12 22:05:47 -0400

    working css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+const deck = {
+    number : ["2","3","4","5","6","7","8","9","10","J","Q","K","A"],
+    shape: ["❤️","♠","☘️","🔹"]
+}
+
 const navToggle = document.querySelector(".nav-toggle");
 const linkContainer = document.querySelector(".link-container")
 const links = document.querySelector(".links")
+const randomNumber = Math.floor(Math.random() * deck.number.length)
+const randomShape = Math.floor(Math.random() * deck.shape.length)
+
 
 navToggle.addEventListener("click", () => {
     const containerHeight = linkContainer.getBoundingClientRect().height;
