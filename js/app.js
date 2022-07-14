2a0e43e8256ce3e303d1a4ef4480ea3976f32db9
DanielJoung <djoung100@hotmail.com>
2022-07-13 22:43:36 -0400

    javascript

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const decks = {
-    rank: [2,3,4,5,6,7,8,9,10,"J","Q","K","A"],
+    rank: ["2","3","4","5","6","7","8","9","10","J","Q","K","A"],
     suit: ["♥︎","♠","♣︎","♦︎"],
     deckOfCard: [],
 }
@@ -10,6 +10,8 @@ for (let i in decks.rank) {
     }
 }
 
+decks.deckOfCard.sort(() => Math.random() - 0.5)
+
 
 // all the html code
 const linkContainer = document.querySelector(".link-container")
@@ -17,8 +19,9 @@ const links = document.querySelector(".links")
 const deckContainer = document.querySelector(".container")
 const potMoney = document.querySelector("#pot_money")
 const userMoney = document.querySelector("#user-money")
-
+const btn = document.querySelector(".btn-container")
 // make card div,p 
+const randomCards = Math.floor(Math.random() * 53) 
 const playerDeck1 = document.createElement("div")
 const playerDeck2 = document.createElement("div")
 const shareDeck1 = document.createElement("div")
@@ -28,7 +31,6 @@ const shareDeck4 = document.createElement("div")
 const shareDeck5 = document.createElement("div")
 const compDeck1 = document.createElement("div")
 const compDeck2 = document.createElement("div")
-const number = document.createElement("p");
 playerDeck1.setAttribute("class","playerDeck1")
 playerDeck2.setAttribute("class","playerDeck2")
 shareDeck1.setAttribute("class","shareDeck1")
@@ -38,11 +40,6 @@ shareDeck4.setAttribute("class","shareDeck4")
 shareDeck5.setAttribute("class","shareDeck5")
 compDeck1.setAttribute("class","compDeck1")
 compDeck2.setAttribute("class","compDeck2")
-number.setAttribute("class","number")
-// number.innerHTML = `${deckOfCard[0]}`
-// deckContainer.appendChild(playerDeck1)
-// playerDeck1.appendChild(number)
-// deckContainer.appendChild(playerDeck2)
 
 // game start 
 class Poker {
@@ -62,9 +59,15 @@ class Poker {
         startBtn.remove()
         // pot money start
         potMoney.innerHTML = `$${this.pot}`
+        this.deal()
+        btn.appendChild(callBtn)
+        btn.appendChild(raiseBtn)
+        btn.appendChild(checkBtn)
+        btn.appendChild(foldBtn)
     }
 
     call() {
+
         if (this.money === 0) {
             alert(`${this.name} don't have enought money, want to use cash shop?`)
         }else {
@@ -72,6 +75,18 @@ class Poker {
             this.pot += 10;
             userMoney.innerHTML = `$${this.money}`
             potMoney.innerHTML = `$${this.pot}`
+        }
+        if(deckContainer.children[6] !== shareDeck4){
+            deckContainer.appendChild(shareDeck4)
+            shareDeck4.innerHTML = `${decks.deckOfCard[5]}`
+        }else if (deckContainer.children[7] !== shareDeck5){
+            deckContainer.appendChild(shareDeck5)
+            shareDeck5.innerHTML = `${decks.deckOfCard[6]}`
+        }else {
+            deckContainer.appendChild(compDeck1)
+            compDeck1.innerHTML = `${decks.deckOfCard[7]}`
+            deckContainer.appendChild(compDeck2)
+            compDeck2.innerHTML = `${decks.deckOfCard[8]}`
         }
     }
 
@@ -84,6 +99,33 @@ class Poker {
             userMoney.innerHTML = `$${this.money}`
             potMoney.innerHTML = `$${this.pot}`
         }
+        if(deckContainer.children[6] !== shareDeck4){
+            deckContainer.appendChild(shareDeck4)
+            shareDeck4.innerHTML = `${decks.deckOfCard[5]}`
+        }else if (deckContainer.children[7] !== shareDeck5){
+            deckContainer.appendChild(shareDeck5)
+            shareDeck5.innerHTML = `${decks.deckOfCard[6]}`
+        }else {
+            deckContainer.appendChild(compDeck1)
+            compDeck1.innerHTML = `${decks.deckOfCard[7]}`
+            deckContainer.appendChild(compDeck2)
+            compDeck2.innerHTML = `${decks.deckOfCard[8]}`
+        }
+    }
+
+    check() {
+        if(deckContainer.children[6] !== shareDeck4){
+            deckContainer.appendChild(shareDeck4)
+            shareDeck4.innerHTML = `${decks.deckOfCard[5]}`
+        }else if (deckContainer.children[7] !== shareDeck5){
+            deckContainer.appendChild(shareDeck5)
+            shareDeck5.innerHTML = `${decks.deckOfCard[6]}`
+        }else {
+            deckContainer.appendChild(compDeck1)
+            compDeck1.innerHTML = `${decks.deckOfCard[7]}`
+            deckContainer.appendChild(compDeck2)
+            compDeck2.innerHTML = `${decks.deckOfCard[8]}`
+        }
     }
 
     fold() {
@@ -91,11 +133,28 @@ class Poker {
         potMoney.innerHTML = `${this.pot}`
     }
 
+    deal() {
+        deckContainer.appendChild(playerDeck1)
+        playerDeck1.innerHTML = `${decks.deckOfCard[0]}`
+
+        deckContainer.appendChild(playerDeck2)
+        playerDeck2.innerHTML = `${decks.deckOfCard[1]}`
+
+        deckContainer.appendChild(shareDeck1)
+        shareDeck1.innerHTML = `${decks.deckOfCard[2]}`
+
+        deckContainer.appendChild(shareDeck2)
+        shareDeck2.innerHTML = `${decks.deckOfCard[3]}`
+
+        deckContainer.appendChild(shareDeck3)
+        shareDeck3.innerHTML = `${decks.deckOfCard[4]}`
+    }
+
+
 }
 
 const poker = new Poker
 poker.pot = 0
-
 
 // all the button
 const navToggle = document.querySelector(".nav-toggle");
@@ -132,3 +191,14 @@ foldBtn.addEventListener("click",e => {
     e.preventDefault()
     poker.fold()
 })
+
+const checkBtn = document.querySelector(".check")
+checkBtn.addEventListener("click",e => {
+    e.preventDefault()
+    poker.check()
+})
+
+callBtn.remove()
+raiseBtn.remove()
+checkBtn.remove()
+foldBtn.remove()
